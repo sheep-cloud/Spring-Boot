733e51e6e6b8747b3fbd48ce51b55899ccc6a409
colg <121529654@qq.com>
2018-04-18 13:55:38 +0800

    add > spring-boot-02-config
    descript : 添加 spring-boot-02-config 工程

---
+++ spring-boot-02-config/src/main/java/com/atguigu/springboot/SpringBoot02ConfigApplication.java
@@ -0,0 +1,17 @@
+package com.atguigu.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring Boot 启动类
+ *
+ * @author colg
+ */
+@SpringBootApplication
+public class SpringBoot02ConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot02ConfigApplication.class, args);
+    }
+}
---
+++ spring-boot-02-config/src/main/java/com/atguigu/springboot/bean/Person.java
@@ -0,0 +1,122 @@
+package com.atguigu.springboot.bean;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 将配置文件中配置的每一个属性的值，映射到这个组件中
+ *
+ * <pre>
+ * ConfigurationProperties：告诉SpringBoot将本类中所有属性和配置文件相关的配置进行绑定；
+ *
+ *  prefix = "person"：配置文件中哪个下面的所有属性进行一一映射
+ *
+ * 只有这个组件是容器中的组件，才能使用提供的ConfigurationProperties功能
+ * </pre>
+ *
+ * @author: colg
+ */
+@Component
+@ConfigurationProperties(prefix = "person")
+//@Validated
+public class Person {
+
+    /**
+     * <bean class="com.atguigu.springboot.bean.Person">
+     *      <property name="lastName" value="字面量">从环境变量，配置文件中获取值/#{SpEL}</property>
+     * </bean>
+     */
+
+    /**
+     * lastName必须是邮箱格式
+     * */
+    @Value("#{person.lastName}")
+    /**
+     * @Email
+     */
+    private String lastName;
+    @Value("#{11*2}")
+    private Integer age;
+    @Value("true")
+    private Boolean boss;
+    private Date birth;
+
+    private Map<String, Object> maps;
+    private List<Object> lists;
+
+    private Dog dog;
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Boolean getBoss() {
+        return boss;
+    }
+
+    public void setBoss(Boolean boss) {
+        this.boss = boss;
+    }
+
+    public Date getBirth() {
+        return birth;
+    }
+
+    public void setBirth(Date birth) {
+        this.birth = birth;
+    }
+
+    public Map<String, Object> getMaps() {
+        return maps;
+    }
+
+    public void setMaps(Map<String, Object> maps) {
+        this.maps = maps;
+    }
+
+    public List<Object> getLists() {
+        return lists;
+    }
+
+    public void setLists(List<Object> lists) {
+        this.lists = lists;
+    }
+
+    public Dog getDog() {
+        return dog;
+    }
+
+    public void setDog(Dog dog) {
+        this.dog = dog;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "lastName='" + lastName + '\'' +
+                ", age=" + age +
+                ", boss=" + boss +
+                ", birth=" + birth +
+                ", maps=" + maps +
+                ", lists=" + lists +
+                ", dog=" + dog +
+                '}';
+    }
+}
