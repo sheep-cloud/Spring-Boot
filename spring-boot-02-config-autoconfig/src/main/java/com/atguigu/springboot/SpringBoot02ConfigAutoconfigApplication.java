eb52b6fa37479d590bda38fc2661e0d3af98a54c
colg <121529654@qq.com>
2018-04-19 02:35:24 +0800

    add > spring-boot-02-config-autoconfig 自动配置
    descript : spring-boot-02-config-autoconfig 自动配置

---
+++ spring-boot-02-config-autoconfig/src/main/java/com/atguigu/springboot/SpringBoot02ConfigAutoconfigApplication.java
@@ -0,0 +1,16 @@
+package com.atguigu.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * @author colg
+ */
+@SpringBootApplication
+public class SpringBoot02ConfigAutoconfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot02ConfigAutoconfigApplication.class, args);
+    }
+}
